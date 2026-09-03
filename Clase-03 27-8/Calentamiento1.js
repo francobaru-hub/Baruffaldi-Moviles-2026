7ce451fa8f222a0caf5cf8e0225d0e369dbff5d2
Axel <castellanoaxl@gmail.com>
2026-09-03 09:27:46 -0300

    feedback: correccion docente Clase 03

--- Clase-03 27-8/Calentamiento1.js
+++ Clase-03 27-8/Calentamiento1.js
@@ -1,3 +1,10 @@
+// ==============================================================================
+// Ejercicio 1: Contraseña Válida
+// Consigna: Escribir una función llamada `contrasenaValida` que reciba un string
+// y retorne `true` si el string es igual a "2Fj(jjbFsuj" o "eoZiugBf&g9".
+// De lo contrario debe retornar `false`.
+// ==============================================================================
+
 function contrasenaValida(str) {
     let retorno = false;
     if (str === "2Fj(jjbFsuj" || str==="eoZiugBf&g9"){
@@ -5,6 +12,14 @@ function contrasenaValida(str) {
     }
   return retorno
 }
+
+//Esto está perfecto, es la manera a la que quiero que se acostumbren en un principio; utilizar una variable auxiliar para usar como retorno según lo que necesite hacer la función. Te dejo otra manera de hacerla:
+
+function contrasenaValida(str) {
+
+    return(str === "2Fj(jjbFsuj" || str==="eoZiugBf&g9")
+    
+  }
 
 console.log(contrasenaValida("2Fj(jjbFsuj")); 
 console.log(contrasenaValida("eoZiugBf&g9")); 
--- Clase-03 27-8/Calentamiento7.js
+++ Clase-03 27-8/Calentamiento7.js
@@ -1,3 +1,10 @@
+// ==============================================================================
+// Ejercicio 7: Número de Caracteres
+// Consigna: Escribir una función llamada `numeroDeCaracteres` que reciba un string
+// y un carácter (un string de longitud 1). La función debe retornar el número de
+// veces que aparece dicho carácter en el string.
+// ==============================================================================
+
 function numeroDeCaracteres(str, caracter) {
     let cantidad = 0;
 
@@ -9,6 +16,17 @@ function numeroDeCaracteres(str, caracter) {
 
     return cantidad;
 }
+
+// Muy bien recorrido el string como un array de caracteres y el acumulador. Te dejo dos alternativas muy utilizadas en JS moderno:
+// Opción A: Separar por el caracter buscado con .split()
+function numeroDeCaracteresA(str, caracter) {
+    return str.split(caracter).length - 1;
+}
+
+// Opción B: Convertir a array con spread [...] y filtrar con .filter()
+const numeroDeCaracteresB = (str, caracter) => [...str].filter(c => c === caracter).length;
+
+// Tanto la opción A, como la B son cosas que vamos a ver en la clase de hoy
 
 console.log(numeroDeCaracteres("Hola Mundo", "o")) 
 console.log(numeroDeCaracteres("MMMMM", "m")) 
